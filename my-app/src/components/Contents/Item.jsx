34fe23bd59e70be7a2c8ff9bc3924ac32d827643
htnghia711 <htnghia711@gmail.com>
2021-02-25 15:14:09 +0700

    component

---
+++ my-app/src/components/Contents/Item.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Item extends Component {
+  render() {
+    return (
+      <>
+        <img
+          className="card-img-top"
+          src="https://picsum.photos/200/300"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </>
+    );
+  }
+}
+export default Item;
